41120be6d428980dd07b4c852b8ad45f1b8fd473
mtgnoah <noahpravecek@gmail.com>
2022-10-21 10:27:23 -0500

    fixs

--- config.py
+++ config.py
@@ -7,26 +7,25 @@ from ethereumetl.utils import split_to_batches
 # Note that there is a limit in Data Pipeline on the number of objects, which can be
 # increased in the Support Center
 # https://docs.aws.amazon.com/datapipeline/latest/DeveloperGuide/dp-limits.html
-EXPORT_PARTITIONS = [(0, 999999)] + \
-                    [(start, end) for start, end in split_to_batches(1000000, 1999999, 100000)] + \
-                    [(start, end) for start, end in split_to_batches(2000000, 2999999, 100000)] + \
-                    [(start, end) for start, end in split_to_batches(3000000, 3999999, 100000)] + \
-                    [(start, end) for start, end in split_to_batches(4000000, 4999999, 10000)]
+EXPORT_PARTITIONS = [(start, end) for start, end in split_to_batches(9000000, 9999999, 1000)] + \
+                    [(start, end) for start, end in split_to_batches(10000000, 10999999, 1000)] 
+
 
 DEFAULT_BUCKET = "example.com"
 
 EXPORT_BLOCKS_AND_TRANSACTIONS = True
-EXPORT_RECEIPTS_AND_LOGS = False
+EXPORT_RECEIPTS_AND_LOGS = True
 EXPORT_CONTRACTS = False
-EXPORT_TOKEN_TRANSFERS = True
-EXPORT_TOKENS = True
+EXPORT_TOKEN_TRANSFERS = False
+EXPORT_TOKENS = False
 
 IS_GETH = False
 
 if IS_GETH:
-    IPC_PATH = 'file:///home/ec2-user/.ethereum/geth.ipc'
+    IPC_PATH = 'https://node.anomalyfi.com'
 else:
-    IPC_PATH = 'file:///home/ec2-user/.local/share/io.parity.ethereum/jsonrpc.ipc'
+    IPC_PATH = 'https://node.anomalyfi.com'
+    
 
 SETUP_COMMAND = \
     "cd /home/ec2-user/ethereum-etl && IPC_PATH={} && ".format(IPC_PATH) + \
@@ -37,10 +36,9 @@ EXPORT_BLOCKS_AND_TRANSACTIONS_COMMAND = SETUP_COMMAND + ' && ' + \
     "--blocks-output ${OUTPUT1_STAGING_DIR}/blocks_${PADDED_START}_${PADDED_END}.csv --transactions-output ${OUTPUT2_STAGING_DIR}/transactions_${PADDED_START}_${PADDED_END}.csv"
 
 EXPORT_RECEIPTS_AND_LOGS_COMMAND = SETUP_COMMAND + ' && ' + \
-    "python3 ethereumetl.py extract_csv_column -i ${INPUT1_STAGING_DIR}/transactions_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp -c hash && " + \
-    "python3 ethereumetl.py export_receipts_and_logs --transaction-hashes ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp -p $IPC_PATH -w 1 " + \
-    "--receipts-output ${OUTPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv --logs-output ${OUTPUT2_STAGING_DIR}/logs_${PADDED_START}_${PADDED_END}.csv && " + \
-    "rm -f ${OUTPUT1_STAGING_DIR}/transaction_hashes.csv.temp"
+    "python3 ethereumetl.py export_receipts_and_logs -s $1 -e $2 -p $IPC_PATH -w 1 " + \
+    "--receipts-output ${OUTPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv --logs-output ${OUTPUT2_STAGING_DIR}/logs_${PADDED_START}_${PADDED_END}.csv && " 
+    
 
 EXPORT_CONTRACTS_COMMAND = SETUP_COMMAND + ' && ' + \
     "python3 ethereumetl.py extract_csv_column -i ${INPUT1_STAGING_DIR}/receipts_${PADDED_START}_${PADDED_END}.csv -o ${OUTPUT1_STAGING_DIR}/contract_addresses.csv.temp -c contract_address && " + \
